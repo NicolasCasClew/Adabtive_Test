e812e198ac012df63f44baec8b75c79f05ea086b
NicolasCasClew <nicolascasclew@gmail.com>
2023-02-07 20:11:09 +0000

    Cleaned code, did more testing and fixed some bugs, comments needes

--- src/Main.java
+++ src/Main.java
@@ -5,60 +5,49 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLACK = "\u001B[30m";
     private static final int maxAccel = 4;  // m/s
     private static final int maxBreak = 10; // m/s
     private static double myCarSpeed = 0;
-    private static double sensorData =0; //sensorData = carDistance????
     private static double carDistance=2;
 
     public static void checkCrash() {
         // check if the distance between the two cars is less than or equal to 0
         if(carDistance<=0){
-            System.out.println("Catastrofe! CRASH");
+            System.out.println(ANSI_RED+"Catastrophe! CRASH!");
             System.exit(0);
         }
     }
 
     public static void main(String[] args) {
-        //double myCarSpeed = 0;
-        //double sensorData =2;
-        //double carDistance=2;
-        ArrayList<Double> speeds = new ArrayList<>();
-        speeds= getSpeeds(speeds);
-        int[] data = new int[]{10,20,50,90,100};
-        /**for(int e:data){  //Just Test ing stuff
-            System.out.println("Data "+e+" de km/s a m/s ="+kmph_to_mps(e));
-            System.out.println("Data "+e+" de m/s a km/h= "+mps_to_kmph(e));
-            System.out.println("Distancia de seguridad a "+e+" kmp = "+sec_distance(e));
-        }*/
-        for(double e:speeds){
-            System.out.println("Speeds="+e);
-        }
-
-            double nowSpeed = 0;
+        double nowSpeed = 0;
         double beforeSpeed=0;
+        ArrayList<Double> speeds = new ArrayList<>();
+
+        speeds= getSpeeds(speeds);
+
         for(double e:speeds){
             nowSpeed = e;
+
             checkCrash();
-            spacer();
 
             carDistance=updateDistance(e,myCarSpeed,carDistance);
-            System.out.println("Distancia = "+ carDistance);            //Test
-            System.out.println("Su velocidad es = "+ e);   //Test
-            System.out.println("Mi velocidad es = "+myCarSpeed);        //Test
+            getData(e);
             if(carDistance >= sec_distance(mps_to_kmph(myCarSpeed))){
                 accelerateCar(e, isBraking(nowSpeed,beforeSpeed));
-                System.out.println("Acelera hombre que se te escapa"); //Test
-                //double desiredDistance = sec_distance(myCarSpeed);
+
+
             }else{
                 brakeCar(e);
-                System.out.println("Quieto parao que te pasas bruto");  //Test
+
             }
-            System.out.println("Now = "+nowSpeed+"   before="+beforeSpeed);
+
             beforeSpeed = nowSpeed;
         }
-        System.out.println("DONE :)");
+        System.out.println(ANSI_GREEN+"Car arrived safely at destination :)");
     }
 
     private static double updateDistance(double car1Speed, double car2Speed, double initialDistance) {
@@ -67,27 +56,21 @@ public class Main {
 
     private static void brakeCar(double otherCarSpeed) {
         if(myCarSpeed>otherCarSpeed){
-            myCarSpeed-=(myCarSpeed-otherCarSpeed);
+            myCarSpeed-=Math.min(myCarSpeed-otherCarSpeed,maxBreak);
         }else{
         myCarSpeed-=Math.min(maxBreak-(sec_distance(mps_to_kmph(myCarSpeed))-carDistance),0);
     }}
 
     private static void accelerateCar(double otherCarSpeed, boolean isBraking) {
-        System.out.println("Acceleracion = "+maxAccel*(((sec_distance(mps_to_kmph(myCarSpeed))*100)/carDistance)/100)+"\n"+
-                "distancia seguridad ="+sec_distance(mps_to_kmph(myCarSpeed)));
         if(otherCarSpeed >= myCarSpeed && (myCarSpeed-otherCarSpeed)<=4){
-            System.out.println("My car speed antes =" + myCarSpeed+"Other car speed = "+ otherCarSpeed);
-            myCarSpeed+=otherCarSpeed-myCarSpeed;
-            System.out.println("Mi car speed=" +myCarSpeed);
+            myCarSpeed+= Math.min(otherCarSpeed-myCarSpeed,maxAccel);
         }else if (isBraking){
-            System.out.println("else fueras"+(otherCarSpeed >= myCarSpeed)+"   "+(Math.abs(otherCarSpeed-myCarSpeed)<4));
+            brakeCar(otherCarSpeed);
             //myCarSpeed += maxAccel;
         }else{
-
+            //mantain speed
         }
     }
-
-
 
     public static double kmph_to_mps(double kmph){
         return round((0.277778 * kmph));
@@ -100,7 +83,7 @@ public class Main {
     }
     public static ArrayList<Double> getSpeeds(ArrayList<Double> speeds){
         try {
-            Scanner sc = new Scanner(new File("src/speeds.txt"));
+            Scanner sc = new Scanner(new File("src/speeds2.txt"));
             while (sc.hasNextLine()) {
                 speeds.add(kmph_to_mps(sc.nextDouble())); //add speeds directly ass m/s??
 
@@ -108,6 +91,7 @@ public class Main {
             sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
+            System.exit(0);
         }
         return speeds;
     }
@@ -115,7 +99,7 @@ public class Main {
         return (Math.round(number*100)/100);
     }
     public static void spacer(){
-        System.out.println("________________________________________________________________");
+        System.out.println(ANSI_BLACK+"________________________________________________________________");
     }
     public static double distanceRatio(){
 
@@ -130,5 +114,11 @@ public class Main {
             return false;
         }
 
+    }
+    public static void getData(double e){
+        System.out.println(ANSI_CYAN+"Distance between cars = "+ carDistance);
+        System.out.println(ANSI_CYAN+"Front car speed = "+ e);
+        System.out.println(ANSI_CYAN+"My car's speed = "+myCarSpeed);
+        spacer();
     }
 }
